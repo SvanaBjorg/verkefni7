62ecd25cc28fa46423b238ba99b10d7f861ad41f
Svana Birgisdottir <svanabirgis@Svanas-MacBook-Air.local>
2021-10-28 22:59:30 +0000

    allt finito

--- scripts.js
+++ scripts.js
@@ -24,39 +24,38 @@ function isValidBestOf(bestOf) {
     if (bestOf %2 === 1 && bestOf <= MAX_BEST_OF) return true ;
     return false;
   }
-//console.assert(isValidBestOf(1) === true, '1 er valid best of');
-//console.assert(isValidBestOf(2) === false, '2 er ekki er valid best of');
-//console.assert(isValidBestOf(9) === true, '9 er valid best of');
+console.assert(isValidBestOf(1) === true, '1 er valid best of');
+console.assert(isValidBestOf(2) === false, '2 er ekki er valid best of');
+console.assert(isValidBestOf(9) === true, '9 er valid best of');
 
 function playAsText(play) {
-    out = 'óþekkt';
+    let out;
     switch (play) {
-        case 1:
-            console.log('Blað');
+        case '1':
+            out = 'Blað';
             break;
 
-        case 2: 
-            console.log('Skæri');
+        case '2': 
+            out = 'Skæri';
             break;
 
-        case 3:
-            console.log('Steinn');
+        case '3':
+            out = 'Steinn'
             break;
 
         default:
-            console.log('Óþekkt inntak');
-            break
+            out = 'Óþekkt';
+            break;
     }
-    console.log(out); 
-    return(out);
-  // TODO útfæra
+    return out;
+
   // Til að geta birt notendanum hvor vann
   // Tökum inn ehv af tölunum fyrir neðan (strengur eða tala) og skilum hvað talan táknar. t.d. 1 == skæri
 }
-//console.assert(playAsText('1') === 'Skæri', '1 táknar skæri');
-//console.assert(playAsText('2') === 'Blað', '2 táknar blað');
-//console.assert(playAsText('3') === 'Steinn', '3 táknar steinn');
-//console.assert(playAsText('foo') === 'Óþekkt', 'Annað er óþekkt');
+console.assert(playAsText('1') === 'Blað', '1 táknar blað');
+console.assert(playAsText('2') === 'Skæri', '2 táknar skæri');
+console.assert(playAsText('3') === 'Steinn', '3 táknar steinn');
+console.assert(playAsText('foo') === 'Óþekkt', 'Annað er óþekkt');
 
 /**
  * Athugar hvort spilari eða tölva vinnur.
@@ -65,60 +64,33 @@ function playAsText(play) {
  * @returns -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function checkGame(player, computer) {
-  let out = 0;
-
-    if (player === "2" && computer === "1") return 1;
-    if (player === "1" && computer === "3") return 1;
-    if (player === "3" && computer === "2") return 1;
+    if (player === '2' && computer === '1') return 1;
+    if (player === '1' && computer === '3') return 1;
+    if (player === '3' && computer === '2') return 1;
     if (player === computer) return 0;
-    else return -1;
+    else return -1; 
 
-
-    /*switch(player){
-        case 'Blað':
-            if (computer === 3) out = 1;
-            else if ( computer === 1) out = 0;
-            else out -1;
-            break;
-
-        case 'Skæri':
-            if (computer === 1) out = 1;
-            else if ( computer === 2) out = 0;
-            else out -1;
-            break;
-
-            case 'Steinn':
-                if (computer === 2) out = 1;
-                else if ( computer === 3) out = 0;
-                else out -1;
-                break;
-            
-            default:
-                out = -1;
-                break;
-    }
-    return out;*/
 }
-//console.assert(checkGame('1', '2') === 1, 'Skæri vinnur blað');
-//console.assert(checkGame('2', '3') === 1, 'Blað vinnur stein');
-//console.assert(checkGame('3', '1') === 1, 'Steinn vinnur skæri');
-//console.assert(checkGame('1', '1') === 0, 'Skæri og skæri eru jafntefli');
-//console.assert(checkGame('1', '3') === -1, 'Skæri tapar fyrir stein');
+console.assert(checkGame('2', '1') === 1, 'Skæri vinnur blað');
+console.assert(checkGame('1', '3') === 1, 'Blað vinnur stein');
+console.assert(checkGame('3', '2') === 1, 'Steinn vinnur skæri');
+console.assert(checkGame('2', '2') === 0, 'Skæri og skæri eru jafntefli');
+console.assert(checkGame('2', '3') === -1, 'Skæri tapar fyrir stein');
 
 /**
  * Spilar einn leik.
  * @return {boolean} -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function round() {
-    const player = prompt('Veldu blað, skæri eða stein');
-    const computer = Math.floor(Math.random() *3) +1 .toString();
+    const player = prompt('Veldu blað, skæri eða stein.');
+    const computer = (Math.floor(Math.random() * 3) + 1).toString();
     let leikur = checkGame(player, computer);
         if (leikur === 1){
-            alert('Þú vannst');
+            alert(`Þú vannst með ${playAsText(player)} á móti ${playAsText(computer)}`);
             wins++;
         }
         if (leikur === -1){
-            alert('Þú tapaðir');
+            alert(`Þú tapaðir með ${playAsText(player)} á móti ${playAsText(computer)}`);
             losses++;
         }
         if (leikur === 0){
@@ -166,10 +138,6 @@ function play() {
  * Birtir stöðu spilara.
  */
 function games() {
-    /*let formulaWins = wins/leikirSpilaðir * 100;
-    let formulaLosses = losses/leikirSpilaðir * 100;
-    alert (leikirSpilaðir + ' Leikir spilaðir ' + formulaWins.toFixed(2) + '%' +' sigurhlutfall');
-    alert (leikirSpilaðir + ' Leikir spilaðir ' + formulaLosses.toFixed(2) + '%' +' taphlutfall');*/
     console.log('Þú hefur spilað', leikirSpilaðir, 'leiki');
     console.log('Þú hefur unnið', wins, ', eða ', wins/leikirSpilaðir*100, '% af heild')
     console.log('Þú hefur tapað', losses, ', eða ', losses/leikirSpilaðir*100, '% af heild')
@@ -177,4 +145,3 @@ function games() {
 }
 // Hér getum við ekki skrifað test þar sem fallið les úr global state
 
-// const text = alert(`jibbí þú vannst ${play?(player)}` )
